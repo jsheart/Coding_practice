b26ae5a9b05b0814651f7b8fc99e3c6513c538f5
Zhen <jsheart@users.noreply.github.com>
2019-09-09 15:47:48 -0700

    binary search

---
+++ wood_cut.py
@@ -0,0 +1,20 @@
+def wood_cut(woods, k):
+    right = max(woods)
+    left = 0
+    longest = 0
+    while left < right:
+        length = left + (right - left) // 2
+        count = 0
+        for wood in woods:
+            count += wood // length
+        if count >= k:
+            longest = max(longest, length)
+            left = length + 1
+        else:
+            right = length - 1
+    return longest
+
+if __name__ == '__main__':
+    woods = [232, 124, 456]
+    k = 7
+    print(wood_cut(woods, k))
